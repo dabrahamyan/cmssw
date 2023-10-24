78b41413a113303526eca8a8b5e0dffba8cf5b74
David Abrahamyan <abrahamyan.d@northeastern.edu>
2023-10-24 12:30:13 +0200

    leftover plotting script added

---
+++ L1Trigger/TrackFindingTracklet/test/myProjects/label_plots_eff_displaced.C
@@ -0,0 +1,66 @@
+//--------------------------------------------------------------------------------------------------------
+// Opens root file in another directory ("dir") which contains histograms and overlays them.
+// In this case, it overlays some property ("prop").
+// It does this for sets of algorithms, parameters, truncations, etc that you can customize.
+// Saves plots to a chosen directory "saveDir"
+//
+// (SetPlotStyle function comes from Louise Skinnari's codes. It's an ATLAS plot style macro)
+//
+// By David Abrahamyan September 2023
+//--------------------------------------------------------------------------------------------------------
+
+#include "plotstyle.h"
+
+void mySmallText(Double_t x, Double_t y, Color_t color, char* text);
+
+void label_plots_eff_displaced (){
+    
+    SetPlotStyle();
+
+    // filename
+    TString fileName = "output_DisplacedMuon_PU0_D88_DISPLACED_LatestDev_2023_10_20";
+    // Load in directory where the root files containing histograms are stored
+    TString dir = "/eos/user/d/dabraham/L1NtupleTrackExamples/";
+    // Property you want to plot
+    TString prop = "eff_";
+    // directory to save plots to
+    TString saveDir = "latestDev_2023_10_20/";
+    
+
+    /////////////////////// CYCLING THROUGH FILES ////////////////////
+    // separate plots for params within algos
+    // vector <TString> params = {"eta", "pt", "phi", "z0", "d0", "absd0"};
+    vector <TString> params = {"z0", "d0"};
+    
+
+    TCanvas c;
+    char ctxt[500];
+    char ctxt2[500];
+
+    TFile *file = new TFile(dir + fileName + ".root");
+
+    for (int iParam = 0; iParam < params.size(); iParam++) {
+        TH1F *hist= (TH1F*)file->Get(prop + params[iParam]);
+
+        hist->Draw();
+
+        sprintf(ctxt, "Displaced Muon PU=0"); // Add label saying 
+        mySmallText(0.2, 0.8, 1, ctxt); // which data set it is
+
+        sprintf(ctxt2, "Hybrid Displaced Tracking"); // Add label saying 
+        mySmallText(0.2, 0.85, 1, ctxt2); // which data set it is
+
+        c.SaveAs(saveDir + "DisplacedMuon_PU0_D88_" + params[iParam] + ".pdf");
+
+        delete hist;
+    }
+}
+
+void mySmallText(Double_t x, Double_t y, Color_t color, char* text) {
+  Double_t tsize = 0.043;
+  TLatex l;
+  l.SetTextSize(tsize);
+  l.SetNDC();
+  l.SetTextColor(color);
+  l.DrawLatex(x, y, text);
+}
